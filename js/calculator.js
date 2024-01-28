f2253e3163f00348be0a4d7a1a753f272ae6610a
=Facinet Kouyate <=fakouyat@student.42wolfsburg.de>
2024-01-28 16:48:18 +0100

    #feat: implemented basic operation

---
+++ js/calculator.js
@@ -0,0 +1,82 @@
+const nonZeroDigits = "123456789";
+const OPERATORS = {
+    add: '+',
+    substract: '-',
+    multiply: 'X',
+    divide: '/',
+    modulo: '%',
+};
+
+const EQUALITY = "operate";
+const SPACE = ' ';
+const CLEAN = 'C';
+
+const add = (num1, num2) => {
+    return num1 + num2;
+}
+
+const substract = (num1, num2) => {
+    return num1 - num2;
+}
+
+const multiply = (num1, num2) => {
+    return num1 * num2; 
+}
+
+const divide = (num1, num2) => {
+    return num1 / num2;
+}
+
+const modulo = (num1, num2) => {
+    return num1 % num2;
+}
+
+const operate = (num1, num2, operation) => {
+    try {
+        const result = operation(num1, num2);
+        return result;
+    }
+    catch (error) {
+        return `ERROR ${error}`;
+    }
+}
+
+const isNonZeroDigit = (key) => {
+    return nonZeroDigits.includes(key);
+}
+
+const isZero = (key) => {
+    return (key == "00" || key == "0");
+}
+
+const isOperator = (key) => {
+    return OPERATORS.hasOwnProperty(key);
+}
+
+const isEquality = (key) => {
+    return key == EQUALITY;
+}
+
+const isCleaning = (key) => {
+    return key == CLEAN;
+}
+
+const Calculator = {
+    add: add,
+    substract: substract,
+    multiply: multiply,
+    divide: divide,
+    modulo: modulo,
+};
+
+export {
+    operate,
+    isNonZeroDigit,
+    isZero,
+    isOperator,
+    isEquality,
+    isCleaning,
+    Calculator,
+    OPERATORS,
+    SPACE,
+};
--- js/index.js
+++ js/index.js
@@ -1,19 +1,96 @@
-const add = (num1, num2) => {
-    return num1 + num2;
+"use strict";
+import {
+    Calculator,
+    isNonZeroDigit,
+    isZero,
+    isOperator,
+    OPERATORS,
+    isEquality,
+    SPACE,
+    operate,
+    isCleaning,
+} from './calculator.js';
+
+const TRANSITION_TIMES = 250;
+const calculation = {
+    num1: "",
+    num2: "",
+    operator: "",
+    result: ""
+};
+
+const touches = document.querySelectorAll('.row_touch');
+const screenOperation = document.querySelector('#screen_operation');
+const screenResult = document.querySelector("#screen_result");
+
+function unselectTouch(touch) {
+    touch.classList.remove('selected');
 }
 
-const subtract = (num1, num2) => {
-    return num1 - num2;
+function animateTouch(touch) {
+    touch.classList.add('selected');
+    setTimeout(() => {
+        unselectTouch(touch);
+    }, TRANSITION_TIMES);
 }
 
-const multiply = (num1, num2) => {
-    return num1 * num2; 
+const updateScreenOperation = (value) => {
+    screenOperation.textContent = value;
 }
 
-const divide = (num1, num2) => {
-    return num1 / num2;
+const cleanScreen = () => {
+    screenOperation.textContent = '0';
+    screenResult.textContent = '';
+    calculation.num1 = "";
+    calculation.num2 = "";
+    calculation.operator = "";
+    calculation.result = "";
+} 
+
+const validPosition = (text, key) => {
+    if (!text.length) return false;
+    if (text[0] == '0') return false;
+    const secondPart = text.lastIndexOf(SPACE);
+    if (secondPart != - 1 && text[secondPart + 1] && isZero(text[secondPart + 1])) {
+        return false; 
+    }
+    if (secondPart != -1 && !text[secondPart + 1] && key.length > 1) return false;
+    return true;
 }
 
-const operate = (num1, num2, operation) => {
-    return operation(num1, num2);
+function handleClick() {
+    animateTouch(this);
+
+    let currentOperation = screenOperation.textContent;
+    const key = this.dataset.key;
+    if (isCleaning(key)) return cleanScreen();
+    if (isNonZeroDigit(key)) {
+        const spaceIndex = currentOperation.lastIndexOf(SPACE);
+        if (isZero(currentOperation)) {
+            currentOperation = key;
+        }
+        else if (isZero(currentOperation[spaceIndex + 1])) {
+            currentOperation = currentOperation.substring(0, spaceIndex + 1) + key;
+        }
+        else currentOperation = currentOperation + key;
+    }
+    else if (isZero(key) && validPosition(currentOperation, key)) currentOperation += key;
+    else if (isOperator(key)) {
+        calculation.num1 = currentOperation;
+        if (!calculation.operator) {
+            currentOperation += ` ${OPERATORS[key]} `;
+            calculation.operator = key;
+        }
+    }
+    else if (isEquality(key)) {
+        const start = currentOperation.lastIndexOf(SPACE);
+        calculation.num2 = currentOperation.substring(start + 1);
+        const result = operate(+calculation.num1, +calculation.num2, Calculator[calculation.operator]);
+        screenResult.textContent = `${result}`;
+    }
+    updateScreenOperation(currentOperation);
 }
+
+touches.forEach((touch) => {
+    touch.addEventListener('click', handleClick);
+});
